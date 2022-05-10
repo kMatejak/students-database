e01af49356acdf8a78341dcd281a78a943be96ca
kMatejak <matejak@tutanota.com>
2022-05-10 17:00:54 +0200

    add test for showing empty db

--- src/database.cpp
+++ src/database.cpp
@@ -1,6 +1,8 @@
 #include "database.hpp"
+#include <iostream>
 
-bool Database::add(const Student & s) {
+bool Database::add(const Student & s) 
+{
     if (isAdded_) {
         return false;
     } else {
@@ -8,3 +10,13 @@ bool Database::add(const Student & s) {
         return true;
     }
 };
+
+void Database::display() const 
+{
+    std::cout << show(); 
+}
+
+std::string Database::show() const
+{
+    return "";
+}
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -16,3 +16,26 @@ TEST(CheckStructure, CanAddStudentToDb_Req1_Req2) {
   EXPECT_TRUE(db.add(json));
   EXPECT_FALSE(db.add(json));
 }
+
+TEST(DisplayDb, DisplayEmptyDatabase) {
+  Database db;
+  auto content = db.show();
+  auto expected = "";
+  EXPECT_EQ(content, expected);
+}
+
+// TEST(DisplayDb, DisplayEmptyDatabase) {
+//   Database db;
+
+//   Student json {
+//     "Json", 
+//     "Kowalski", 
+//     "ul. Bobrza 45, 21-370 Warszawa",
+//     134,
+//     "12345678901",
+//     Gender::Male
+//   };
+//   EXPECT_TRUE(db.add(json));
+
+//   db.display();
+// }
