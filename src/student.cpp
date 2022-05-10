d1c2e44608b73d4aa0d3ef7be63aea3eafa922db
kMatejak <matejak@tutanota.com>
2022-05-10 18:50:46 +0200

    add showing student's data

--- src/student.cpp
+++ src/student.cpp
@@ -4,7 +4,25 @@ Student::Student(
     std::string name,
     std::string lastName,
     std::string address,
-    int indexNumber, 
+    int indexNumber,
     std::string pesel,
     Gender gender)
-{};
+
+    : name_(name),
+      lastName_(lastName),
+      address_(address),
+      indexNumber_(indexNumber),
+      pesel_(pesel),
+      gender_(gender)
+{
+}
+
+std::string Student::show() const
+{
+    return name_ + " " 
+         + lastName_ + "; " 
+         + address_ + "; " 
+         + std::to_string(indexNumber_) + "; " 
+         + pesel_ + "; " 
+         + "Male";
+};
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -2,24 +2,10 @@
 #include "../database.hpp"
 #include "../student.hpp"
 
-struct DatabaseTest : ::testing::Test 
+struct DatabaseTest : ::testing::Test
 {
   Database db;
-}
-
-TEST_F(DatabaseTest, CanAddStudentToDb_Req1_Req2) {
-  Student json{
-    "Json", 
-    "Kowalski", 
-    "ul. Bobrza 45, 21-370 Warszawa",
-    134,
-    "12345678901",
-    Gender::Male
-  };
-
-  EXPECT_TRUE(db.add(json));
-  EXPECT_FALSE(db.add(json));
-}
+};
 
 TEST_F(DatabaseTest, DisplayEmptyDatabase) {
   auto content = db.show();
@@ -27,18 +13,19 @@ TEST_F(DatabaseTest, DisplayEmptyDatabase) {
   EXPECT_EQ(content, expected);
 }
 
-// TEST(DisplayDb, DisplayEmptyDatabase) {
-//   Database db;
+TEST_F(DatabaseTest, DisplayNonEmptyDatabase) {
+  Student json {
+    "Json", 
+    "Kowalski", 
+    "ul. Bobrza 45, 21-370 Warszawa",
+    134,
+    "12345678901",
+    Gender::Male
+  };
+  db.add(json);
+  // check adding the same person twice
 
-//   Student json {
-//     "Json", 
-//     "Kowalski", 
-//     "ul. Bobrza 45, 21-370 Warszawa",
-//     134,
-//     "12345678901",
-//     Gender::Male
-//   };
-//   EXPECT_TRUE(db.add(json));
-
-//   db.display();
-// }
+  auto content = db.show();
+  auto expected = "Json Kowalski; ul. Bobrza 45, 21-370 Warszawa; 134; 12345678901; Male";
+  EXPECT_EQ(content, expected);
+}
